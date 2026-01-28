9bb425d22ba15f32305dd4686bc731e26df6c244
Владислав Степанов <vstepanov@web-bee.ru>
2026-01-28 23:56:03 +0900

    feat: добавлена демонстрация гонки потоков (Race Condition)

---
+++ src/main/java/io/github/vladislav/Multithreading/RaceConditionDemo.java
@@ -0,0 +1,39 @@
+package io.github.vladislav.Multithreading;
+
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.IntStream;
+
+/**
+ * Задача 9 "Демонстрация гонки потоков (Race Condition)"
+ * @author Владислав Степанов
+ */
+public class RaceConditionDemo {
+
+    private static int counter = 0;
+    private static final LongAdder longAdder = new LongAdder(); // Также можно реализовать через AtomicInteger или Synchronized блоки
+
+    public static void main(String[] args) {
+
+        IntStream.range(0, 10).forEach(i -> {
+            Thread thread = new Thread(() -> {
+                IntStream.range(0, 1000).forEach(x -> {
+                    counter++;
+                });
+            });
+            thread.start();
+        });
+
+        IntStream.range(0, 10).forEach(i -> {
+            Thread thread = new Thread(() -> {
+                IntStream.range(0, 1000).forEach(x -> {
+                    longAdder.increment();
+                });
+            });
+            thread.start();
+        });
+
+        System.out.println("Counter: " + counter); // Ожидание – 10000
+        System.out.println("LongAdder: " + longAdder);
+
+    }
+}

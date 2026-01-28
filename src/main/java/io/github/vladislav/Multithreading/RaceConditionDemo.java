5d28c7fb362e1b1c82df9fece86ecfc45dc84dc6
Владислав Степанов <vstepanov@web-bee.ru>
2026-01-29 00:00:57 +0900

    feat: добавлено ожидание завершения потоков

--- src/main/java/io/github/vladislav/Multithreading/RaceConditionDemo.java
+++ src/main/java/io/github/vladislav/Multithreading/RaceConditionDemo.java
@@ -1,5 +1,7 @@
 package io.github.vladislav.Multithreading;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
 
@@ -12,14 +14,15 @@ public class RaceConditionDemo {
     private static int counter = 0;
     private static final LongAdder longAdder = new LongAdder(); // Также можно реализовать через AtomicInteger или Synchronized блоки
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
 
         IntStream.range(0, 10).forEach(i -> {
             Thread thread = new Thread(() -> {
-                IntStream.range(0, 1000).forEach(x -> {
-                    counter++;
-                });
+                IntStream.range(0, 1000).forEach(x -> counter++);
             });
+            threads.add(thread);
             thread.start();
         });
 
@@ -29,8 +32,13 @@ public class RaceConditionDemo {
                     longAdder.increment();
                 });
             });
+            threads.add(thread);
             thread.start();
         });
+
+        for (Thread t : threads) {
+            t.join();
+        }
 
         System.out.println("Counter: " + counter); // Ожидание – 10000
         System.out.println("LongAdder: " + longAdder);

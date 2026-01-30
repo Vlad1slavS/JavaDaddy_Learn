e20e019471c438ef983fa0aa250c517739826ab9
Владислав Степанов <vstepanov@web-bee.ru>
2026-01-30 18:33:40 +0900

    feat: реализована демонстрация паттерна Producer-Consumer

--- src/main/java/io/github/vladislav/Multithreading/ProducerConsumerDemo.java
+++ src/main/java/io/github/vladislav/Multithreading/ProducerConsumerDemo.java
@@ -1,8 +1,52 @@
 package io.github.vladislav.Multithreading;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+/**
+ * Задача 6 "Producer-Consumer с wait/notify"
+ * @author Владислав Степанов
+ */
 public class ProducerConsumerDemo {
-    public static void main(String[] args) {
+
+    static Random random = new Random();
+    static Integer MAX_BUFFER_SIZE = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Integer> buffer = new ArrayList<>();
+
+        Thread producer = new Thread(() -> {
+            while (true) {
+                synchronized (buffer) {
+                    while (buffer.size() != MAX_BUFFER_SIZE) {
+                        Integer value = random.nextInt(100);
+                        buffer.add(value);
+                        System.out.println("Записал " + value);
+                    }
+                    buffer.notifyAll();
+                }
+            }
+        });
+
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                synchronized (buffer) {
+                    while (!buffer.isEmpty()) {
+                        int item = buffer.removeFirst();
+                        System.out.println("Прочитал " + item);
+                    }
+                    buffer.notifyAll();
+                }
+            }
+        });
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
 
     }
 }
